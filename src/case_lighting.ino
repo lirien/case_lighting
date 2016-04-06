5398c5d1a07d45cc4f466732095425aa3d7d7fa1
Erin Sullivan <kittylily@gmail.com>
2016-04-06 16:43:54 -0700

    made it so pulse wasn't SO BRIGHT

--- src/case_lighting.ino
+++ src/case_lighting.ino
@@ -16,13 +16,13 @@
 #define PULSE     2
 
 #define FADESPEED 5
-
-
+#define SAT       17
 
 volatile int buttonState = HIGH;  //unpressed
 volatile int previousButtonState = HIGH;
 
 int mode = 0;
+
 
 void setup() {
   pinMode(REDPIN, OUTPUT);
@@ -47,11 +47,12 @@ void setup() {
 
 
 void loop() {
-  int hval, bval;
+  int bval, hval, bscale;
   int r, g, b, w;
 
   bval = analogRead(BKNOB) / 10;
-  hval = analogRead(HKNOB) / 10;
+  hval = analogRead(HKNOB);
+
 
   switch(mode){
     case WHITE:
@@ -61,45 +62,45 @@ void loop() {
       analogWrite(WHITEPIN, bval);
       break;
     case COLOR:
-      analogWrite(GREENPIN,hal);
-      analogWrite(BLUEPIN,0);
-      analogWrite(WHITEPIN, 0);
       analogWrite(REDPIN,0);
+      analogWrite(GREENPIN,30);
+      analogWrite(BLUEPIN,30);
+      analogWrite(WHITEPIN, 0);
       break;
     case PULSE:
     // fade from blue to violet
     for (r = 0; r < 256; r++) {
-      analogWrite(REDPIN, r);
+      analogWrite(REDPIN, r / 10);
       delay(FADESPEED);
       if(mode != PULSE) break;
     }
     // fade from violet to red
     for (b = 255; b > 0; b--) {
-      analogWrite(BLUEPIN, b);
+      analogWrite(BLUEPIN, b / 10);
       delay(FADESPEED);
       if(mode != PULSE) break;
     }
     // fade from red to yellow
     for (g = 0; g < 256; g++) {
-      analogWrite(GREENPIN, g);
+      analogWrite(GREENPIN, g / 10);
       delay(FADESPEED);
       if(mode != PULSE) break;
     }
     // fade from yellow to green
     for (r = 255; r > 0; r--) {
-      analogWrite(REDPIN, r);
+      analogWrite(REDPIN, r / 10);
       delay(FADESPEED);
       if(mode != PULSE) break;
     }
     // fade from green to teal
     for (b = 0; b < 256; b++) {
-      analogWrite(BLUEPIN, b);
+      analogWrite(BLUEPIN, b / 10);
       delay(FADESPEED);
       if(mode != PULSE) break;
     }
     // fade from teal to blue
     for (g = 255; g > 0; g--) {
-      analogWrite(GREENPIN, g);
+      analogWrite(GREENPIN, g / 10);
       delay(FADESPEED);
       if(mode != PULSE) break;
     }
