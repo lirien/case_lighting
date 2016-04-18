2cec670856fd9d4786c7e65984580e7a18784021
Erin Sullivan <kittylily@gmail.com>
2016-04-17 17:10:06 -0700

    oh, this is much better. hsi with white

--- src/case_lighting.ino
+++ src/case_lighting.ino
@@ -1,5 +1,5 @@
 // case lighting!
-#include "../lib/case_lighting.h"
+#include "../lib/hsi2rgbw.h"
 
 #define REDPIN    5
 #define GREENPIN  6
@@ -14,7 +14,7 @@
 
 #define HKNOB 1
 #define SKNOB 2
-#define VKNOB 3
+#define IKNOB 3
 
 #define WHITE     0
 #define COLOR     1
@@ -54,135 +54,20 @@ void setup() {
 
 void loop() {
 
-  HsvColor hsv;
-  RgbColor rgb;
+  float h, s, i;
+  RgbwColor rgbw;
 
-  hsv.h = analogRead(HKNOB) / 4;
-  hsv.s = analogRead(SKNOB) / 4;
-  hsv.v = analogRead(VKNOB) / 4;
+  h = (float) analogRead(HKNOB) / 2.84;
+  s = (float) analogRead(SKNOB) / 1024.0;
+  i = (float) analogRead(IKNOB) / 1024.0;
 
-  rgb = HsvToRgb(hsv);
+  hsi2rgbw(h,s,i,&rgbw);
 
-  analogWrite(WHITEPIN,0);
-  analogWrite(REDPIN, rgb.r);
-  analogWrite(GREENPIN,rgb.g);
-  analogWrite(BLUEPIN,rgb.b);
+  analogWrite(WHITEPIN,rgbw.w);
+  analogWrite(REDPIN, rgbw.r);
+  analogWrite(GREENPIN,rgbw.g);
+  analogWrite(BLUEPIN,rgbw.b);
 
-  // int bval, hval, bscale;
-  // int rgbscale;
-  // int fr, fg, fb;
-
-
-  // bval = analogRead(BKNOB) / BSCALE;
-  // hval = analogRead(HKNOB);
-  // rgbscale = 168 / bval;
-
-
-  // switch(mode){
-  //   case WHITE:
-  //     analogWrite(REDPIN,0);
-  //     analogWrite(GREENPIN,0);
-  //     analogWrite(BLUEPIN,0);
-  //     analogWrite(WHITEPIN, bval);
-  //     break;
-  //   case COLOR:
-  //
-  //     analogWrite(WHITEPIN,0);
-  //     //off
-  //     if(bval == 0){
-  //       analogWrite(REDPIN,0);
-  //       analogWrite(GREENPIN,0);
-  //       analogWrite(BLUEPIN,0);
-  //     }
-  //
-  //     //blue to violet
-  //     else if(hval < 172){
-  //       analogWrite(GREENPIN,0);
-  //       analogWrite(BLUEPIN, bval);
-  //       // r = hval;
-  //       analogWrite(REDPIN, hval / rgbscale);
-  //     }
-  //
-  //     //violet to red
-  //     else if(hval < 340){
-  //       analogWrite(GREENPIN,0);
-  //       analogWrite(REDPIN, bval);
-  //       //b = 85 - hval;
-  //       analogWrite(BLUEPIN, (340 - hval) / rgbscale);
-  //     }
-  //
-  //     //red to yellow
-  //     else if(hval < 512){
-  //       analogWrite(BLUEPIN,0);
-  //       analogWrite(REDPIN, bval);
-  //       //g = hval - 85
-  //       analogWrite(GREENPIN, (hval - 340) / rgbscale);
-  //     }
-  //
-  //     //yellow to green
-  //     else if(hval < 680){
-  //       analogWrite(BLUEPIN,0);
-  //       analogWrite(GREENPIN, bval);
-  //       //r = 170 - hval;
-  //       analogWrite(REDPIN, (680 - hval) / rgbscale);
-  //     }
-  //
-  //     //green to teal
-  //     else if(hval < 852){
-  //       analogWrite(REDPIN,0);
-  //       analogWrite(GREENPIN, bval);
-  //       //b = hval - 170
-  //       analogWrite(BLUEPIN, (hval - 680) / rgbscale);
-  //     }
-  //
-  //     //teal to blue
-  //     else{
-  //       analogWrite(REDPIN,0);
-  //       analogWrite(BLUEPIN, bval);
-  //       //g = 255 - hval;
-  //       analogWrite(GREENPIN, (1027 - hval) / rgbscale);
-  //     }
-  //     break;
-  //
-  //   case PULSE:
-  //   // fade from blue to violet
-  //   for (fr = 0; fr < 256; fr++) {
-  //     analogWrite(REDPIN, fr / BSCALE);
-  //     delay(FADESPEED);
-  //     if(mode != PULSE) break;
-  //   }
-  //   // fade from violet to red
-  //   for (fb = 255; fb > 0; fb--) {
-  //     analogWrite(BLUEPIN, fb / BSCALE);
-  //     delay(FADESPEED);
-  //     if(mode != PULSE) break;
-  //   }
-  //   // fade from red to yellow
-  //   for (fg = 0; fg < 256; fg++) {
-  //     analogWrite(GREENPIN, fg / BSCALE);
-  //     delay(FADESPEED);
-  //     if(mode != PULSE) break;
-  //   }
-  //   // fade from yellow to green
-  //   for (fr = 255; fr > 0; fr--) {
-  //     analogWrite(REDPIN, fr / BSCALE);
-  //     delay(FADESPEED);
-  //     if(mode != PULSE) break;
-  //   }
-  //   // fade from green to teal
-  //   for (fb = 0; fb < 256; fb++) {
-  //     analogWrite(BLUEPIN, fb / BSCALE);
-  //     delay(FADESPEED);
-  //     if(mode != PULSE) break;
-  //   }
-  //   // fade from teal to blue
-  //   for (fg = 255; fg > 0; fg--) {
-  //     analogWrite(GREENPIN, fg / BSCALE);
-  //     delay(FADESPEED);
-  //     if(mode != PULSE) break;
-  //   }
-  //     break;
-  // }
 
 
 }
